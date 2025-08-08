5ad0ecc5c1a800baca697453dee3f69a2f86642f
IcedDog <34804287+IcedDog@users.noreply.github.com>
2025-08-08 16:16:18 +0800

    plugin inject + pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
-    base: '/visual-works/'
+    base: '/visual-works/',
+    plugins: [
+        inject({
+            p5: 'p5',
+        }),
+    ],
 })
